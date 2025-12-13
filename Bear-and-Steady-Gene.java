89424ecf9f3c44bc990db39bda656bd521563e69
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-12-13 12:30:45 +0900

    Update Bear-and-Steady-Gene.java

--- Bear-and-Steady-Gene.java
+++ Bear-and-Steady-Gene.java
@@ -20,3 +20,24 @@ class Result {
      * The function accepts STRING gene as parameter.
      */
 }
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String gene = bufferedReader.readLine();
+
+        int result = Result.steadyGene(gene);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
